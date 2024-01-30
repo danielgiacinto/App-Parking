e652f862481c2a0b9420b22e4f481ccd4d837153
114253-Giacinto-Daniel <danielgiacinto@gmail.com>
2024-01-30 18:15:54 -0300

    cambios en historical
    agregar tabla con datos

--- frontEnd/src/app/components/historial/historial.component.ts
+++ frontEnd/src/app/components/historial/historial.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { format } from 'date-fns';
 import { Car } from 'src/app/Model/Car';
+import { Payment } from 'src/app/Model/Payment';
 import { CarsService } from 'src/app/services/cars.service';
+import { PaymentsService } from 'src/app/services/payments.service';
 
 @Component({
   selector: 'app-historial',
@@ -9,9 +12,10 @@ import { CarsService } from 'src/app/services/cars.service';
 })
 export class HistorialComponent implements OnInit {
 
-  constructor(private carsService: CarsService) { }
+  constructor(private carsService: CarsService, private paymentsService: PaymentsService) { }
   originalCarsHistorical: Car[] = [];
   carsHistorical: Car[] = [];
+  payments: Payment[] = [];
   fechaDesde: Date = new Date();
   fechaHasta: Date = new Date();
 
@@ -23,51 +27,61 @@ export class HistorialComponent implements OnInit {
         this.carsHistorical = [...this.originalCarsHistorical];
       }
     );
+    this.loadPayments().subscribe(
+      (data) => {
+        console.log(data);
+        this.payments = data;
+      }
+    );
   }
 
   loadCarsHistorical() {
     return this.carsService.getCarsHistorical();
   }
 
-  ordenarPor(event: any) {
-    const valor = event.target.value;
-
-    if (valor == 2) {
-      this.carsHistorical = [...this.originalCarsHistorical].reverse();
-    } else if (valor == 1) {
-      this.carsHistorical = [...this.originalCarsHistorical];
-    } else {
-      this.carsHistorical = [...this.originalCarsHistorical];
-
-      if (valor == 3) {
-        this.carsHistorical = this.carsHistorical.filter((car) => String(car.type) === 'Automovil');
-      } else if (valor == 4) {
-        this.carsHistorical = this.carsHistorical.filter((car) => String(car.type) === 'Camioneta');
-      } else if (valor == 5) {
-        this.carsHistorical = this.carsHistorical.filter((car) => String(car.type) === 'Motocicleta');
-      }
-    }
+  loadPayments() {
+    return this.paymentsService.getPayments();
   }
 
   buscarPorFechas() {
     if (this.fechaDesde && this.fechaHasta) {
-      // Filtra los vehículos por fechas
       this.carsHistorical = this.originalCarsHistorical.filter((car) =>
         car.admissionDate >= this.fechaDesde && car.dischargeDate <= this.fechaHasta
       );
+      console.log(this.fechaDesde, this.fechaHasta);
+      console.log('Lista fitrada por fechas', this.carsHistorical);
   
-      // Aplicar los filtros adicionales si ya se aplicaron antes
-      this.ordenarPor(event);
-    } else {
-      // Si alguna fecha no está seleccionada, muestra un mensaje o realiza la lógica que desees
-      console.log("Por favor, selecciona ambas fechas.");
-  
-      // Restaurar la lista original si no hay fechas seleccionadas
-      this.carsHistorical = [...this.originalCarsHistorical];
+      const selectedFormat = document.getElementById("format") as HTMLSelectElement;
+      const selectedFormatId = selectedFormat.value;
+      console.log(selectedFormatId);
       
-      // Aplicar los filtros adicionales si ya se aplicaron antes
-      this.ordenarPor(event);
+      if (selectedFormatId && selectedFormatId == "1") {
+        this.carsHistorical = this.carsHistorical.filter((car) => String(car.format) == 'Efectivo');
+        console.log('Lista fitrada por efectivo', this.carsHistorical);
+      } else if(selectedFormatId && selectedFormatId == "2") {
+        this.carsHistorical = this.carsHistorical.filter((car) => String(car.format) == 'Tarjeta de Credito');
+        console.log('Lista fitrada por tarjeta credito', this.carsHistorical);
+      } else if(selectedFormatId && selectedFormatId == "3") {
+        this.carsHistorical = this.carsHistorical.filter((car) => String(car.format) == 'Tarjeta de Debito');
+        console.log('Lista fitrada por tarjeta debito', this.carsHistorical);
+      } else if (selectedFormatId && selectedFormatId == "4") {
+        this.carsHistorical = this.carsHistorical.filter((car) => String(car.format) == 'Mercado Pago');
+        console.log('Lista fitrada por mp', this.carsHistorical);
+      } else {
+        console.log('Lista sin filtrar', this.carsHistorical);
+      }
+  
     }
+  }
+
+  limpiar() {
+    this.carsHistorical = [...this.originalCarsHistorical];
+    this.fechaDesde = new Date();
+    this.fechaHasta = new Date();
+  }
+
+  formatearFecha(fecha: string): string {
+    return format(new Date(fecha), 'HH:mm,  dd-MM-yyyy');
   }
   
 }
